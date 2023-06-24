03006dbac4200cf112ba33b70aa46fa9830abdc2
Michael Liou <lioumens@gmail.com>
2023-06-24 14:01:46 -0500

    first version working w/ beta likelihood

--- src/components/sandbox/BetaLikelihood.vue
+++ src/components/sandbox/BetaLikelihood.vue
@@ -4,13 +4,17 @@
 // import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
 // import MathBox from "mathbox"
 
-import {onMounted, ref, computed, toValue} from "vue"
+import {onMounted, ref, computed, toValue, watch, watchEffect} from "vue"
 import * as d3 from "d3"
 import { seq, seqn } from "../../lib/util.js"
+import {Matrix} from "ml-matrix"
+import { x } from "../../../dist/_astro/runtime-core.esm-bundler.a7f258e4";
 
 
 const xGrid = ref(seqn(-5, 5, 100))
 const yGrid = ref(seqn(-5, 5, 100))
+const X = ref(new Matrix([[1, 1],[1, 2],[1, 3],[1, 4]]))
+const y = ref([1, 0, 1, 1])
 const zGrid = computed(() => 
     outer(xGrid.value, yGrid.value, (x,y) => x + y + Math.random())
 )
@@ -53,14 +57,43 @@ const tester = ref()
 
 function updatePlot() {
     xGrid.value = xGrid.value.map(x => x + 1)
+    // const vuedata = [{
+    //     x: xGrid.value,
+    //     y: yGrid.value,
+    //     z: zGrid.value,
+    //     type: 'surface',
+    // }]
+    // Plotly.react(tester.value, vuedata, layout, {displaylogo: false, responsive:true, displayModeBar:false,scrollZoom: true})
+}
+
+
+function betall(b0, b1) {
+    // console.log(X.value.clone())
+    // console.log([b0, b1])
+    // console.log(Matrix.rowVector([b0, b1]))
+    const eta = X.value.clone().mmul(Matrix.columnVector([b0, b1])).getColumn(0)
+    const mui = eta.map(x => 1 / (1 + Math.exp(-x)))
+    const sumll = mui.map((x,i) => y.value[i] * Math.log(x) + (1 - y.value[i]) * Math.log(1 - x)).reduce((a,b) => a+b, 0)
+    return(sumll)
+}
+const calcZ = computed(() => {        
+    return(outer(xGrid.value, yGrid.value, betall))
+})
+
+watchEffect(() => {
+    console.log("trying to change")
     const vuedata = [{
         x: xGrid.value,
         y: yGrid.value,
-        z: zGrid.value,
+        z: calcZ.value,
         type: 'surface',
     }]
-    Plotly.react(tester.value, vuedata, layout, {displaylogo: false, responsive:true, displayModeBar:false,scrollZoom: true})
-}
+    console.log(calcZ.value)
+    tester.value ? Plotly.react(tester.value, vuedata, layout, {displaylogo: false, responsive:true, displayModeBar:false,scrollZoom: true}) : null
+})
+
+
+
 
 
 onMounted(() => {
@@ -71,7 +104,6 @@ onMounted(() => {
         });
 
         const TESTER = document.getElementById('tester')
-        console.log(TESTER)
         // const data = [{
         //     x: [...Array(24).keys()].map(i => i / 24),
         //     y: [...Array(24).keys()].map(i => i / 24),
@@ -88,10 +120,9 @@ onMounted(() => {
         const vuedata = [{
             x: xGrid.value,
             y: yGrid.value,
-            z: zGrid.value,
+            z: calcZ.value,
             type: 'surface',
         }]
-        console.log(toValue(tester))
         Plotly.react(tester.value, vuedata, layout, {displaylogo: false, responsive:true, displayModeBar:false,scrollZoom: true})
     })    //     const TESTER = document.getElementById('tester');
     // 	Plotly.newPlot( TESTER, [{
